cca87cd187ed3578718b38c34e393dc32f1916fa
Kirill Kalashnikov <kirill310587@mail.ru>
2021-12-25 15:20:14 +0700

    Modify readme

--- install.sh
+++ install.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-release=$1
-task=$2
-
-git branch "$release"
-git pull origin "$release"
-git checkout -b "$task"
 bash ./vendor/bin/sail up &
 bash ./vendor/bin/sail composer install
 bash ./vendor/bin/sail npm install
